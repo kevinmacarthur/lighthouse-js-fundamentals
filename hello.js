d13c0b64d17e061f2776b01bb450483dc98ee206
kevinmacarthur <kmacarthur16@gmail.com>
2018-08-22 21:09:52 +0000

    Adds functions exercises

---
+++ hello.js
@@ -0,0 +1,12 @@
+function sayHello(name) {
+  console.log("Hello, " + name);
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+function returnSayHello(name) {
+  return "Hello, " + name;
+}
+var greeting = returnSayHello('John');
+console.log(greeting);
