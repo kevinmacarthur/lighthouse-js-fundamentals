c10306c6b17297b84a396e7d394c1903e2569a27
kevinmacarthur <kmacarthur16@gmail.com>
2018-08-29 17:36:06 +0000

    added looping ingredients practice

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var repeat = 0;
+
+while (repeat < ingredients.length) {
+  console.log(ingredients[repeat]);
+  repeat ++
+};
+
+for (i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i])
+};
+
+for (j = (ingredients.length - 1); j >=0; j--) {
+  console.log(ingredients[j])
+};
